55780464d7df8233601b1faea871265d6c4b4ca1
Yuriy Ivanov <ymivanov@icloud.com>
2021-07-30 17:50:21 +0300

    implement a private <setBackground> function to auto set parallax backgrounds depending on scene width

--- src/scenes/CityLevelScene.ts
+++ src/scenes/CityLevelScene.ts
@@ -1,5 +1,13 @@
 import Phaser from "../lib/phaser";
 
+
+type BackgroundOpts = {
+  position?: { posX?: number; posY?: number; };
+  scale?: { scaleX?: number; scaleY?: number; };
+  origin?: { originX?: number; originY?: number; };
+  scrollFactor?: { scrollFactorX?: number; scrollFactorY?: number };
+};
+
 export default class CityLevelScene extends Phaser.Scene {
   private width: number;
   private height: number;
@@ -22,15 +30,34 @@ export default class CityLevelScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(this.width * 0.5, this.height * 0.5, "cbFarBuildings").setScale(3.1).setScrollFactor(0.1);
-    const mid = this.add.image(0, this.height * 0.9, "cbMidBuildings").setOrigin(0, 1).setScale(3).setScrollFactor(0.5);
-    this.add.image(mid.width * 3, this.height * 0.9, "cbMidBuildings").setOrigin(0, 1).setScale(3).setScrollFactor(0.5);
-
-    const front = this.add.image(0, this.height, "cbForeground").setOrigin(0, 1).setScale(2).setScrollFactor(1);
-    this.add.image(front.width * 2, this.height, "cbForeground").setOrigin(0, 1).setScale(2).setScrollFactor(1);
-    this.add.image(front.width * 4, this.height, "cbForeground").setOrigin(0, 1).setScale(2).setScrollFactor(1);
+    // main bacground image, should barely scroll //
+    this.setBackgrounds(
+      { scene: this, imageKey: "cbFarBuildings" }, 
+      { origin: { originX: 0, originY: 0 }, 
+        scale: { scaleX: 3.1, scaleY: 3.1 }, 
+        scrollFactor: { scrollFactorX: 0.1, scrollFactorY: 0.1 } 
+      }
+    );
+    // mid background city image, faster scroll //
+    this.setBackgrounds(
+      { scene: this, imageKey: "cbMidBuildings" },
+      { position: { posX: 0, posY: this.height * 0.9 }, 
+        origin: { originX: 0, originY: 1 }, 
+        scale: { scaleX: 3, scaleY: 3 }, 
+        scrollFactor: { scrollFactorX: 0.5, scrollFactorY: 0.5 } 
+      }
+    );
+    // player bacground, should scroll with player //
+    this.setBackgrounds(
+      { scene: this, imageKey: "cbForeground" },
+      { position: { posX: 0, posY: this.height }, 
+        origin: { originX: 0, originY: 1 }, 
+        scale: { scaleX: 2, scaleY: 2 }, 
+        scrollFactor: { scrollFactorX: 1, scrollFactorY: 1 } 
+      }
+    );
     // camera //
-    this.cameras.main.setBounds(0, 0, this.width * 3, this.height);
+    this.cameras.main.setBounds(0, 0, this.width * 4, this.height);
   }
 
   update() {
@@ -43,5 +70,31 @@ export default class CityLevelScene extends Phaser.Scene {
       mainCam.scrollX += cameraSpeed;
     }
   }
+
+  
+  private setBackgrounds({ scene, imageKey }: { scene: Phaser.Scene; imageKey: string }, imageOpts?: BackgroundOpts): void {
+    console.log(scene.cameras.main.getBounds())
+    const { position = {}, scale = {}, origin = {}, scrollFactor = {} } = imageOpts ? imageOpts : {}
+    // positioning //
+    const { posX = 0, posY = 0 } = position;
+    // scale //
+    const { scaleX = 1, scaleY = 1 } = scale;
+    // origin //
+    const { originX = 0.5, originY = 0.5 } = origin;
+    // scroll factor //
+    const { scrollFactorX = 1, scrollFactorY = 1 } = scrollFactor;
+    let currentBGSpan = posX;
+    do {
+      let bgImgTemp = scene.add.image(currentBGSpan, posY, imageKey)
+        .setOrigin(0, 0)
+        .setScale(scaleX, scaleY)
+        .setOrigin(originX, originY)
+        .setScrollFactor(scrollFactorX, scrollFactorY);
+      currentBGSpan = currentBGSpan + (bgImgTemp.width * scaleX);
+
+    } while (currentBGSpan < (this.width * 4))
+  }
 };
+
+
 
